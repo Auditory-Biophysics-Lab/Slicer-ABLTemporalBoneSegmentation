03968e695216ca02b94930e036e06189ee19800b
Evan Simpson <esimps27@uwo.ca>
2019-08-13 16:43:37 -0400

    Furthered Skull Thickness Mapping
    Implemented STL file parsing
    Added facet class
    Furthered processing/'main'
    Added sample images & stl files

--- SkullThicknessMapping/SkullThicknessMapping.py
+++ SkullThicknessMapping/SkullThicknessMapping.py
@@ -1,5 +1,9 @@
+import inspect
+
 import slicer
 import numpy
+import itertools
+import re
 from slicer.ScriptedLoadableModule import *
 
 
@@ -20,12 +24,49 @@ class SkullThicknessMappingWidget(ScriptedLoadableModuleWidget):
 
     def setup(self):
         ScriptedLoadableModuleWidget.setup(self)
+        SkullThicknessMappingLogic.process("1601L")
+
+
+class Facet:
+    n = None
+    v1 = None
+    v2 = None
+    v3 = None
+
+    def __init__(self, n, v1, v2, v3):
+        self.n = n
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "\n  [Facet]" + " n:" + str(self.n) + "\n          v1:" + str(self.v1) + "\n          v2:" + str(self.v2) + "\n          v3:" + str(self.v3) + '\n'
 
 
 class SkullThicknessMappingLogic(ScriptedLoadableModuleLogic):
     @staticmethod
-    def process(files):
-        pass
+    def sample_folder():
+        return slicer.os.path.dirname(slicer.os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/Resources/Sample/'
+
+    @staticmethod
+    def process(sample_name, parameters=None):
+        parameters = {
+          'n_dura_voxel': 20,
+          'thresh_bone': 600,
+          'thresh_aircell': 150,
+          'thresh_dura': -300,
+          'n_outward_bone_check': 5
+        }
+        uniD = 10
+        data = {
+          'image_name': SkullThicknessMappingLogic.sample_folder() + 'Images/' + sample_name + '.hdr',
+          'outer_stl_data': SkullThicknessMappingLogic.read_stl(SkullThicknessMappingLogic.sample_folder() + 'STL/' + sample_name + '_outer.stl'),
+          # 'rest_stl_data': SkullThicknessMappingLogic.read_stl(SkullThicknessMappingLogic.sample_folder() + 'STL/' + sample_name + '_rest.stl')
+        }
+        # print(data)
         # for each
         # get file names for image, outer, and rest of ear
         # read stl files for outer and rest of ear
@@ -33,87 +74,83 @@ class SkullThicknessMappingLogic(ScriptedLoadableModuleLogic):
         # plot output
         # return output
 
-
-class SkullThicknessMappingAlgorithms:
     @staticmethod
-    def amanatidesWooAlgorithm(origin=None, direction=None, image=None, grid_3d_nx=None, grid_3d_ny=None,
-                               grid_3d_nz=None, grid_3d_min_bound=None, grid_3d_max_bound=None):
+    def amanatidesWooAlgorithm(origin=None, direction=None, image=None, grid_3d_nx=None, grid_3d_ny=None, grid_3d_nz=None, grid_3d_min_bound=None, grid_3d_max_bound=None):
         # A fast and simple voxel traversal algorithm through a 3D space partition(grid)
         # Proposed by J.Amanatides and A.Woo(1987)
         # Input:    origin, direction, grid3d (grid dimensions nx, ny, nz, minBound, maxBound)
         # Author:   Jesus Mena
 
-        flag, tMin = SkullThicknessMappingAlgorithms.rayBoxIntersection(origin, direction, grid_3d_min_bound,
-                                                                        grid_3d_max_bound, nargout=2)
+        flag, tMin = SkullThicknessMappingLogic.rayBoxIntersection(origin, direction, grid_3d_min_bound, grid_3d_max_bound, nargout=2)
         if flag == 0:
-            print('[!] The ray does not intersect the grid')
-        else:
-            if tMin < 0: tMin = 0
-            start = origin + numpy.floor(tMin, direction)
-            boxSize = grid_3d_max_bound - grid_3d_min_bound
-            x = numpy.floor(numpy.dot(((start(1) - grid_3d_min_bound(1)) / boxSize(1)), grid_3d_nx)) + 1
-            y = numpy.floor(numpy.dot(((start(2) - grid_3d_min_bound(2)) / boxSize(2)), grid_3d_ny)) + 1
-            z = numpy.floor(numpy.dot(((start(3) - grid_3d_min_bound(3)) / boxSize(3)), grid_3d_nz)) + 1
-            if x == (grid_3d_nx + 1): x = x - 1
-            if y == (grid_3d_ny + 1): y = y - 1
-            if z == (grid_3d_nz + 1): z = z - 1
-            if direction(1) >= 0:
-                tVoxelX = x / grid_3d_nx
-                stepX = 1
-            else:
-                tVoxelX = (x - 1) / grid_3d_nx
-                stepX = - 1
-            if direction(2) >= 0:
-                tVoxelY = y / grid_3d_ny
-                stepY = 1
-            else:
-                tVoxelY = (y - 1) / grid_3d_ny
-                stepY = - 1
-            if direction(3) >= 0:
-                tVoxelZ = z / grid_3d_nz
-                stepZ = 1
-            else:
-                tVoxelZ = (z - 1) / grid_3d_nz
-                stepZ = - 1
+          print('[!] The ray does not intersect the grid')
+          return None, None
 
-            voxelMaxX = grid_3d_min_bound(1) + numpy.dot(tVoxelX, boxSize(1))
-            voxelMaxY = grid_3d_min_bound(2) + numpy.dot(tVoxelY, boxSize(2))
-            voxelMaxZ = grid_3d_min_bound(3) + numpy.dot(tVoxelZ, boxSize(3))
-            tMaxX = tMin + (voxelMaxX - start(1)) / direction(1)
-            tMaxY = tMin + (voxelMaxY - start(2)) / direction(2)
-            tMaxZ = tMin + (voxelMaxZ - start(3)) / direction(3)
-            voxelSizeX = boxSize(1) / grid_3d_nx
-            voxelSizeY = boxSize(2) / grid_3d_ny
-            voxelSizeZ = boxSize(3) / grid_3d_nz
-            tDeltaX = voxelSizeX / abs(direction(1))
-            tDeltaY = voxelSizeY / abs(direction(2))
-            tDeltaZ = voxelSizeZ / abs(direction(3))
-            i, t_flag = 1, 1
-            while (x <= grid_3d_nx) and (x >= 1) and (y <= grid_3d_ny) and (y >= 1) and (z <= grid_3d_nz) and (
-                    z >= 1) and t_flag:
-                intersection_voxel = [x, y, z]
-                voxel_value = image(y, x, z)
-                t1 = [(x - 1) / grid_3d_nx, (y - 1) / grid_3d_ny, (z - 1) / grid_3d_nz]
-                t2 = [x / grid_3d_nx, y / grid_3d_ny, z / grid_3d_nz]
-                # vMin = (grid_3d_min_bound + numpy.multiply(t1, boxSize))
-                # vMax = (grid_3d_min_bound + numpy.multiply(t2, boxSize))
-                i = i + 1
-                # check if voxel [x,y,z] contains any intersection with the ray
-                #   if ( intersection ) break
-                if tMaxX < tMaxY:
-                    if tMaxX < tMaxZ:
-                        x = x + stepX
-                        tMaxX = tMaxX + tDeltaX
-                    else:
-                        z = z + stepZ
-                        tMaxZ = tMaxZ + tDeltaZ
+        if tMin < 0: tMin = 0
+        start = origin + numpy.floor(tMin, direction)
+        boxSize = grid_3d_max_bound - grid_3d_min_bound
+        x = numpy.floor(numpy.dot(((start(1) - grid_3d_min_bound(1)) / boxSize(1)), grid_3d_nx)) + 1
+        y = numpy.floor(numpy.dot(((start(2) - grid_3d_min_bound(2)) / boxSize(2)), grid_3d_ny)) + 1
+        z = numpy.floor(numpy.dot(((start(3) - grid_3d_min_bound(3)) / boxSize(3)), grid_3d_nz)) + 1
+        if x == (grid_3d_nx + 1): x = x - 1
+        if y == (grid_3d_ny + 1): y = y - 1
+        if z == (grid_3d_nz + 1): z = z - 1
+        if direction(1) >= 0:
+            tVoxelX = x / grid_3d_nx
+            stepX = 1
+        else:
+            tVoxelX = (x - 1) / grid_3d_nx
+            stepX = - 1
+        if direction(2) >= 0:
+            tVoxelY = y / grid_3d_ny
+            stepY = 1
+        else:
+            tVoxelY = (y - 1) / grid_3d_ny
+            stepY = - 1
+        if direction(3) >= 0:
+            tVoxelZ = z / grid_3d_nz
+            stepZ = 1
+        else:
+            tVoxelZ = (z - 1) / grid_3d_nz
+            stepZ = - 1
+
+        voxelMaxX = grid_3d_min_bound(1) + numpy.dot(tVoxelX, boxSize(1))
+        voxelMaxY = grid_3d_min_bound(2) + numpy.dot(tVoxelY, boxSize(2))
+        voxelMaxZ = grid_3d_min_bound(3) + numpy.dot(tVoxelZ, boxSize(3))
+        tMaxX = tMin + (voxelMaxX - start(1)) / direction(1)
+        tMaxY = tMin + (voxelMaxY - start(2)) / direction(2)
+        tMaxZ = tMin + (voxelMaxZ - start(3)) / direction(3)
+        voxelSizeX = boxSize(1) / grid_3d_nx
+        voxelSizeY = boxSize(2) / grid_3d_ny
+        voxelSizeZ = boxSize(3) / grid_3d_nz
+        tDeltaX = voxelSizeX / abs(direction(1))
+        tDeltaY = voxelSizeY / abs(direction(2))
+        tDeltaZ = voxelSizeZ / abs(direction(3))
+        i, t_flag = 1, 1
+        while (x <= grid_3d_nx) and (x >= 1) and (y <= grid_3d_ny) and (y >= 1) and (z <= grid_3d_nz) and (z >= 1) and t_flag:
+            intersection_voxel = [x, y, z]
+            voxel_value = image(y, x, z)
+            # t1 = [(x - 1) / grid_3d_nx, (y - 1) / grid_3d_ny, (z - 1) / grid_3d_nz]
+            # t2 = [x / grid_3d_nx, y / grid_3d_ny, z / grid_3d_nz]
+            # vMin = (grid_3d_min_bound + numpy.multiply(t1, boxSize))
+            # vMax = (grid_3d_min_bound + numpy.multiply(t2, boxSize))
+            i = i + 1
+            # check if voxel [x,y,z] contains any intersection with the ray
+            #   if ( intersection ) break
+            if tMaxX < tMaxY:
+                if tMaxX < tMaxZ:
+                    x = x + stepX
+                    tMaxX = tMaxX + tDeltaX
                 else:
-                    if tMaxY < tMaxZ:
-                        y = y + stepY
-                        tMaxY = tMaxY + tDeltaY
-                    else:
-                        z = z + stepZ
-                        tMaxZ = tMaxZ + tDeltaZ
+                    z = z + stepZ
+                    tMaxZ = tMaxZ + tDeltaZ
+            else:
+                if tMaxY < tMaxZ:
+                    y = y + stepY
+                    tMaxY = tMaxY + tDeltaY
+                else:
+                    z = z + stepZ
+                    tMaxZ = tMaxZ + tDeltaZ
         return intersection_voxel, voxel_value
 
     @staticmethod
@@ -169,8 +206,25 @@ class SkullThicknessMappingAlgorithms:
         # end;
         return flag, tMin
 
-    # @staticmethod
-    # def read_stl(file_name=None):
+    @staticmethod
+    def read_stl(file_name):
+        data = []
+        with open(file_name, mode='r') as file:
+            print("Start: " + file_name)
+            file.read(1)
+            for lines in itertools.izip_longest(*[iter(file)] * 7, fillvalue=None):
+                assert len(lines) == 7
+                print("facet " + str(len(data)+1) + ": " + str(lines))
+                data.append(Facet(
+                    n=[float(s) for s in re.findall(r"[-+]?\d*\.\d+|\d+", lines[1])],
+                    v1=[float(s) for s in re.findall(r"[-+]?\d*\.\d+|\d+", lines[3])],
+                    v2=[float(s) for s in re.findall(r"[-+]?\d*\.\d+|\d+", lines[4])],
+                    v3=[float(s) for s in re.findall(r"[-+]?\d*\.\d+|\d+", lines[5])]
+                ))
+                slicer.app.processEvents()
+                break
+        print(data)
+
         # # open file
         # fileOpen = fopen(file_name, 'r')
         # format = '%*s %*s %f32 %f32 %f32 \\n %*s %*s \\n %*s %f32 %f32 %f32 \\n %*s %f32 %f32 %f32 \\n %*s %f32 %f32 %f32 \\n %*s \\n %*s \\n'
